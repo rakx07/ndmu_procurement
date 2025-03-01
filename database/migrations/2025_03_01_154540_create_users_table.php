672bd5ba94310c481dfa36dcd9e4103065f46948
Janry S <janryseran069407@gmail.com>
2025-03-02 00:28:29 +0800

    added users table updated and deleted the default

--- database/migrations/2025_03_01_154540_create_users_table.php
+++ database/migrations/2025_03_01_154540_create_users_table.php
@@ -9,7 +9,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('employee_id')->unique();
+            $table->string('lastname');
+            $table->string('firstname');
+            $table->string('middlename')->nullable();
             $table->string('email')->unique();
             $table->string('password');
             $table->integer('role')->default(0); // 0: Staff, 1: Purchasing Officer, 2: Supervisor, 3: Admin, 4: Comptroller, 5: IT Admin

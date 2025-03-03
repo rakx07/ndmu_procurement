88c20d5c4c5d42e0c6317a7d72650fb16fc5e055
janry <janryseran069407@gmail.com>
2025-03-03 10:08:07 +0800

    updated seeder

--- database/seeders/OfficeSeeder.php
+++ database/seeders/OfficeSeeder.php
@@ -10,11 +10,17 @@ class OfficeSeeder extends Seeder
     public function run()
     {
         DB::table('offices')->insert([
-            ['name' => 'IT Department', 'description' => 'Handles all IT-related tasks.'],
-            ['name' => 'Accounting Office', 'description' => 'Manages financial transactions.'],
-            ['name' => 'Procurement Office', 'description' => 'Handles purchase and procurement.'],
+            ['name' => 'ICT Department', 'description' => 'Handles all IT-related tasks.'],
+            ['name' => 'Business Office', 'description' => 'Manages financial transactions.Handles purchase and procurement.'],
             ['name' => 'HR Department', 'description' => 'Manages human resources and hiring.'],
             ['name' => 'Administration', 'description' => 'Oversees school-wide operations.'],
+            ['name' => 'CAS Office', 'description' => 'College of Arts and Sciences'],
+            ['name' => 'CEAC Office', 'description' => 'College of Engineering ,Architecture and Computing'],
+            ['name' => 'CED Office', 'description' => 'College of Education'],
+            ['name' => 'CBA Office', 'description' => 'College of Business Administration'],
+            ['name' => 'PPO Office', 'description' => 'Physical Plant Office'],
+            ['name' => 'CCC Office', 'description' => 'Champagnat Community COllege'],
+            ['name' => 'QAPS', 'description' => 'Quality Assurance Office'],
         ]);
     }
 }
